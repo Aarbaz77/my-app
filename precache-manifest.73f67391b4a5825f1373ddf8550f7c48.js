bcaf393a366dcd9b69abf23081a48dbe0adf8c9d
Aarbaz Alam <aarbazlm6@gmail.com>
2019-03-10 00:52:13 +0000

    Updates

---
+++ precache-manifest.73f67391b4a5825f1373ddf8550f7c48.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "acb2881ff3f6d78a0471",
+    "url": "/static/css/main.e236fdec.chunk.css"
+  },
+  {
+    "revision": "acb2881ff3f6d78a0471",
+    "url": "/static/js/main.acb2881f.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1854f9d1315c0789af33",
+    "url": "/static/js/2.1854f9d1.chunk.js"
+  },
+  {
+    "revision": "6c516cc3b80015fd9d26f8dbf57d23c0",
+    "url": "/static/media/PartlyCloudy.6c516cc3.png"
+  },
+  {
+    "revision": "cd5558c9215de959711ccac439fad671",
+    "url": "/static/media/Sun.cd5558c9.png"
+  },
+  {
+    "revision": "2c8f9cd2783dca232e4a43700eed4d35",
+    "url": "/static/media/Heavy Rain.2c8f9cd2.png"
+  },
+  {
+    "revision": "49ed916b16636e29e43b9de0eb7ce9d7",
+    "url": "/static/media/COF.49ed916b.png"
+  },
+  {
+    "revision": "f9f4abf1be1ceacc7e03d032d233385b",
+    "url": "/static/media/Precipitation.f9f4abf1.png"
+  },
+  {
+    "revision": "e80c5f19c90590c90b020a095f7ac3eb",
+    "url": "/static/media/wind.e80c5f19.svg"
+  },
+  {
+    "revision": "55500e748f92fc7a0143859055849a58",
+    "url": "/static/media/visibility.55500e74.svg"
+  },
+  {
+    "revision": "72eac0734e358ea04e75535f2022e109",
+    "url": "/static/media/Sunrise.72eac073.png"
+  },
+  {
+    "revision": "9b13bf99a1333ea2b5726c3f934f2aa2",
+    "url": "/static/media/Sunset.9b13bf99.png"
+  },
+  {
+    "revision": "f731ab6ab14e307d87a1e613375f843a",
+    "url": "/static/media/pressure.f731ab6a.svg"
+  },
+  {
+    "revision": "0ef6cda4afb74505365774e41009248f",
+    "url": "/static/media/UV.0ef6cda4.png"
+  },
+  {
+    "revision": "f2a3f2f1e2c2a23a91eca5fd83afdc7b",
+    "url": "/static/media/humidity (1).f2a3f2f1.svg"
+  },
+  {
+    "revision": "065caea3e2faf0cbe430bd233ef5d480",
+    "url": "/static/media/Background.065caea3.jpg"
+  },
+  {
+    "revision": "f60dfc67a0cf3cfdace80d40cf571c9a",
+    "url": "/index.html"
+  }
+];
