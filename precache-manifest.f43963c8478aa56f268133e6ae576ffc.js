032f7c59bb838533877f4194e4e189b7106242e4
Aarbaz Alam <aarbazlm6@gmail.com>
2019-03-10 22:07:28 +0000

    Updates

---
+++ precache-manifest.f43963c8478aa56f268133e6ae576ffc.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "777d29f3e820289c4de5",
+    "url": "/my-app/static/css/main.303094a5.chunk.css"
+  },
+  {
+    "revision": "777d29f3e820289c4de5",
+    "url": "/my-app/static/js/main.777d29f3.chunk.js"
+  },
+  {
+    "revision": "caff8f4450a1e5b1c09b",
+    "url": "/my-app/static/js/runtime~main.caff8f44.js"
+  },
+  {
+    "revision": "00364de4f4bd79606b8b",
+    "url": "/my-app/static/js/2.00364de4.chunk.js"
+  },
+  {
+    "revision": "cd5558c9215de959711ccac439fad671",
+    "url": "/my-app/static/media/Sun.cd5558c9.png"
+  },
+  {
+    "revision": "e18664578eec3f3f78a0ff296ae798a8",
+    "url": "/my-app/static/media/Red_Warning.e1866457.png"
+  },
+  {
+    "revision": "910fed2e0624dee98182a21bc0b39b67",
+    "url": "/my-app/static/media/Cloudy.910fed2e.png"
+  },
+  {
+    "revision": "2c8f9cd2783dca232e4a43700eed4d35",
+    "url": "/my-app/static/media/Heavy_Rain.2c8f9cd2.png"
+  },
+  {
+    "revision": "aff6f320b90c39d05765d34c8a62c425",
+    "url": "/my-app/static/media/Light_Rain.aff6f320.png"
+  },
+  {
+    "revision": "c081aaa29d36489e904d96ae28133c80",
+    "url": "/my-app/static/media/Moon.c081aaa2.png"
+  },
+  {
+    "revision": "1b86357e8226a66f60a42a6c34b258ee",
+    "url": "/my-app/static/media/Snow.1b86357e.png"
+  },
+  {
+    "revision": "e713e0e52becca01d270e87ce6240b62",
+    "url": "/my-app/static/media/PartlyCloudyMoon.e713e0e5.png"
+  },
+  {
+    "revision": "6c516cc3b80015fd9d26f8dbf57d23c0",
+    "url": "/my-app/static/media/PartlyCloudySunny.6c516cc3.png"
+  },
+  {
+    "revision": "dab3ddbaf7d89bcd6acdfc8ec9550d41",
+    "url": "/my-app/static/media/Warning.dab3ddba.png"
+  },
+  {
+    "revision": "b14f395124345de22f7d24f945a8d0df",
+    "url": "/my-app/static/media/Green_Warning.b14f3951.png"
+  },
+  {
+    "revision": "cf0eb46660ae656ae8c51f7e59f03bb1",
+    "url": "/my-app/static/media/Day.cf0eb466.jpg"
+  },
+  {
+    "revision": "04ca4892ad5900f1cf002f710c525004",
+    "url": "/my-app/static/media/Night.04ca4892.jpg"
+  },
+  {
+    "revision": "a4afa29ccaa3a78ca289c2ba391aa25f",
+    "url": "/my-app/static/media/location.a4afa29c.svg"
+  },
+  {
+    "revision": "49ed916b16636e29e43b9de0eb7ce9d7",
+    "url": "/my-app/static/media/COF.49ed916b.png"
+  },
+  {
+    "revision": "f9f4abf1be1ceacc7e03d032d233385b",
+    "url": "/my-app/static/media/Precipitation.f9f4abf1.png"
+  },
+  {
+    "revision": "8a3449959513593a69c9521902a22c0f",
+    "url": "/my-app/static/media/visibility.8a344995.svg"
+  },
+  {
+    "revision": "72eac0734e358ea04e75535f2022e109",
+    "url": "/my-app/static/media/Sunrise.72eac073.png"
+  },
+  {
+    "revision": "9b13bf99a1333ea2b5726c3f934f2aa2",
+    "url": "/my-app/static/media/Sunset.9b13bf99.png"
+  },
+  {
+    "revision": "6debf5db1db3b1075cf2cc32768ea0c1",
+    "url": "/my-app/static/media/Cloud_Day.6debf5db.jpg"
+  },
+  {
+    "revision": "97ab3d7accb79b3901a489fb444cfbd6",
+    "url": "/my-app/static/media/Cloud_Night.97ab3d7a.jpg"
+  },
+  {
+    "revision": "ce811ea6f0d53af6c3dd5f48f8cea2bd",
+    "url": "/my-app/static/media/pressure.ce811ea6.svg"
+  },
+  {
+    "revision": "aac74e0d72cc8f3c42ac4aed8c37a6d9",
+    "url": "/my-app/static/media/humidity (1).aac74e0d.svg"
+  },
+  {
+    "revision": "cf0eb46660ae656ae8c51f7e59f03bb1",
+    "url": "/my-app/static/media/Background.cf0eb466.jpg"
+  },
+  {
+    "revision": "cc594825efd05a453081c25de6ddf008",
+    "url": "/my-app/index.html"
+  }
+];
